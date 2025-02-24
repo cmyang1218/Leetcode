1ae65921aa643eee4ef5b11fca754c1245fd35dd
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-02-24 10:01:53 +0800

    Time: 8 ms (81.11%) | Memory: 11.9 MB (53.88%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,14 +12,14 @@ public:
         if (head == nullptr) {
             return false;
         }
-        ListNode *slow = head, *fast = head->next;
-        while (slow != fast) {
-            if (fast == nullptr || fast->next == nullptr) {
-                return false;
-            }
+        ListNode* slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
         }
-        return true;
+        return false;
     }
 };
