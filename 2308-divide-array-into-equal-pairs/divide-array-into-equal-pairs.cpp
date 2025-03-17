739276443633d691fe02a0f98b4e4912573b0900
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-03-17 13:29:11 +0800

    Time: 8 ms (13.53%) | Memory: 18.3 MB (9.58%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        map<int, int> numMap;
+        for (auto num : nums) {
+            numMap[num]++;
+        }
+        bool ret = true;
+        for (auto n : numMap) {
+            if (n.second % 2 != 0) {
+                ret = false;
+                break;
+            }
+        }
+        return ret;
+    }
+};
