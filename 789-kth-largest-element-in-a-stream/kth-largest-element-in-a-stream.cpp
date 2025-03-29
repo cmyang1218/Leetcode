f199a71bae6b8224fb749419bd482495375430da
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-03-29 12:02:17 +0800

    Time: 11 ms (31.53%) | Memory: 33.3 MB (8.02%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -21,12 +21,11 @@ public:
 
 private: 
     int findIndex(int val) {
-         
-        int left = 0, right = this->nums.size();
-        while (left < right) {
+        int left = 0, right = this->nums.size()-1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (val < this->nums[mid]) {
-                right = mid;
+                right = mid - 1;
             }else {
                 left = mid + 1;
             }
