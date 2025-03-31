c94e269c01242f375e14915661027cccb2208750
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-03-31 09:21:11 +0800

    Time: 32 ms (81.43%) | Memory: 67.4 MB (56.54%) - LeetSync

---
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        vector<int> pairWeights;
+        size_t n = weights.size();
+        for (int i = 0; i < n-1; i++) {
+            pairWeights.push_back(weights[i] + weights[i+1]);
+        }
+        sort(pairWeights.begin(), pairWeights.end());
+        long long maxScore = weights[0] + weights[n-1];
+        long long minScore = weights[0] + weights[n-1];
+        for (int i = 0; i < k-1; i++) {
+            minScore += pairWeights[i];
+        }
+        for (int i = 0; i < k-1; i++) {
+            maxScore += pairWeights[n-i-2];
+        }
+        cout << minScore << " " << maxScore << endl;;
+        return maxScore - minScore;
+    }
+};
