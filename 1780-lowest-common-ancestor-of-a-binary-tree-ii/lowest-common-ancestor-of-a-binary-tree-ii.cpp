f75bad7a9c2ec3c63fb5f169be9b024479a90786
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-04-04 15:18:28 +0800

    Time: 94 ms (85.45%) | Memory: 62 MB (92.02%) - LeetSync

---
+++ 1780-lowest-common-ancestor-of-a-binary-tree-ii/lowest-common-ancestor-of-a-binary-tree-ii.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool found_p = false, found_q = false;
+
+    TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root)
+            return nullptr;
+
+        TreeNode* left = LCA(root->left, p, q);
+        TreeNode* right = LCA(root->right, p, q);
+        if (root == p) {
+            found_p = true;
+            return root;
+        }
+        if (root == q) {
+            found_q = true;
+            return root;
+        }
+        if (left != nullptr && right != nullptr) {
+            if (left == p) {
+                found_p = true;
+            }
+            if (right == q) {
+                found_q = true;
+            }
+            return root;
+        }
+        if (left != nullptr) {
+            return left;
+        }
+        if (right != nullptr) {
+            return right;
+        }
+        return nullptr;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* ancestor = LCA(root, p, q);
+        if (found_p && found_q)
+            return ancestor;
+        return nullptr;
+    }
+};
