1759ef285f8c96bd378cca87863f3f70b7c3fd14
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-04-08 10:29:35 +0900

    Time: 775 ms (5.80%) | Memory: 28.4 MB (35.50%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    bool countFrequency(vector<int>& nums) {
-        unordered_map<int, int> u_map;
-        for (int i = 0; i < nums.size(); i++) {
-            u_map[nums[i]]++;
-        }
-        for (auto& it : u_map) {
-            if (it.second != 1) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     int minimumOperations(vector<int>& nums) {
+        unordered_map<int, int> u_map;
+        for (auto& num : nums) {
+            u_map[num]++;
+        }
         int cnt = 0;
-        while (!nums.empty() && !countFrequency(nums)) {
-            if (nums.size() < 3)
+        while (!nums.empty()) {
+            cout << nums.size() << endl;
+            for (auto& it : u_map) {
+                cout << it.first << ' ' << it.second << endl;
+                if (it.second > 1) {
+                    cnt++;
+                    break;
+                }
+            }
+            if (nums.size() < 3) {
+                for (auto& num : nums) {
+                    u_map[num]--;
+                }
                 nums.erase(nums.begin(), nums.end());
-            else
+            }else {
+                for (int i = 0; i < 3; i++) {
+                    u_map[nums[i]]--;
+                }
                 nums.erase(nums.begin(), nums.begin() + 3);
-            cnt++;
+            }
         }
         return cnt;
     }
