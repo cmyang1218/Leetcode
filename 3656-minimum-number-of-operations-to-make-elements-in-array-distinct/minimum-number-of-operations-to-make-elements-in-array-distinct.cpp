916b6154a51a31878e3f7ae6c6ae7d2ae7a86cd0
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-04-08 10:40:55 +0900

    Time: 0 ms (100.00%) | Memory: 27 MB (96.93%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -2,32 +2,13 @@ class Solution {
 public:
 
     int minimumOperations(vector<int>& nums) {
-        unordered_map<int, int> u_map;
-        for (auto& num : nums) {
-            u_map[num]++;
-        }
-        int cnt = 0;
-        while (!nums.empty()) {
-            cout << nums.size() << endl;
-            for (auto& it : u_map) {
-                cout << it.first << ' ' << it.second << endl;
-                if (it.second > 1) {
-                    cnt++;
-                    break;
-                }
+        vector<bool> visited(101, false);
+        for (int i = nums.size()-1; i >= 0; i--) {
+            if (visited[nums[i]]) {
+                return i / 3 + 1;
             }
-            if (nums.size() < 3) {
-                for (auto& num : nums) {
-                    u_map[num]--;
-                }
-                nums.erase(nums.begin(), nums.end());
-            }else {
-                for (int i = 0; i < 3; i++) {
-                    u_map[nums[i]]--;
-                }
-                nums.erase(nums.begin(), nums.begin() + 3);
-            }
+            visited[nums[i]] = true;
         }
-        return cnt;
+        return 0;
     }
 };
