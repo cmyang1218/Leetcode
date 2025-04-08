1011e44fc4a1387e5017fd3f26f2dba3ea407b1c
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-04-08 11:33:39 +0900

    Time: 95 ms (72.77%) | Memory: 13.3 MB (91.02%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % 2)
+            return false;
+        int target = total >> 1;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = target; j >= nums[i]; j--) {
+
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+            cout << endl;
+        }
+        return dp[target];
+    }
+};
