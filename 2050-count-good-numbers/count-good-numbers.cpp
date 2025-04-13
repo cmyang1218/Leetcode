0332ba3a396954452ec77dc3ce4b4ae1bf6948d7
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-04-13 22:59:03 +0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.99%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     const int MOD = 1e9 + 7;
-    int fast_exp(long long x, long long n) {
-        long long ans = 1;
-        long long base = x;
+    long long fast_exp(long long x, long long n) {
+        long long ret = 1, base = x;
         while (n != 0) {
-            if (n & 1) {
-                ans = ans * base % MOD;
-            }
+            if (n & 1) 
+                ret = ret * base % MOD;
             base = base * base % MOD;
             n >>= 1;
         }
-        return ans;
+        return ret;
     }
 
     int countGoodNumbers(long long n) {
-        long long oddIdxs = n / 2;
         long long evenIdxs = (n + 1) / 2;
-        return (long long)fast_exp(5, evenIdxs) * fast_exp(4, oddIdxs) % MOD;
+        long long oddIdxs = n / 2;
+        return fast_exp(5, evenIdxs) * fast_exp(4, oddIdxs) % MOD;
     }
 };
