cff4211c1b7f101e1401d325a59e5d4e7beed969
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-06-17 23:09:05 +0800

    Time: 0 ms (100.00%) | Memory: 9.8 MB (57.77%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        string ans;
         if (str1 + str2 != str2 + str1) {
-            ans = "";
-        }else {
-            int len = gcd(str1.length(), str2.length());
-            ans = (str1.length() > str2.length()) ? str1.substr(0, len) : str2.substr(0, len);   
+            return "";
         }
-        return ans;
+        int len = gcd(str1.length(), str2.length());
+        return str1.substr(0, len);
     }
 };
