705ee72daf26e456599452313e84f46248bc2d5f
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-06-17 22:42:50 +0800

    Time: 2 ms (43.23%) | Memory: 8.4 MB (83.63%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string str;
-        str.resize(word1.length() + word2.length());
-        int idx1 = 0, idx2 = 0, cnt = 0;
-        while(idx1 < word1.length() && idx2 < word2.length()) {
-            if (cnt % 2 == 0) {
-                str[cnt++] = word1[idx1++]; 
+        string res;
+        int cnt = 0;
+        int idx1 = 0, idx2 = 0;
+        while (idx1 < word1.length() && idx2 < word2.length()) {
+            if (cnt % 2) {
+                res += word2[idx2++];
             }else {
-                str[cnt++] = word2[idx2++];
-            }   
+                res += word1[idx1++];
+            }
+            cnt++;
         }
-        while (idx1 == word1.length() && idx2 < word2.length()) {
-            str[cnt++] = word2[idx2++];
+        if (idx1 < word1.length()) {
+            res += word1.substr(idx1, word1.length()-idx1);
+        }else if (idx2 < word2.length()) {
+            res += word2.substr(idx2, word2.length()-idx2);
         }
-        while (idx1 < word1.length() && idx2 == word2.length()) {
-            str[cnt++] = word1[idx1++];
-        }
-        return str;
+        return res;
     }
 };
