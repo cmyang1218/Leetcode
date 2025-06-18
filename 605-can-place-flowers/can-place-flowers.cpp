d3ff040debd5ec0bf1da1a34af5a7d9c5ff42645
YANG, CHUO-MIN <58680451+cmyang1218@users.noreply.github.com>
2025-06-18 16:33:44 +0800

    Time: 0 ms (100.00%) | Memory: 24 MB (97.42%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int legalCnt = 0;
+        int size = flowerbed.size();
+        int placable = 0;
         for (int i = 0; i < flowerbed.size(); i++) {
             bool emptyLeft = (i == 0) || (flowerbed[i-1] == 0);
-            bool emptyRight = (i == flowerbed.size()-1) || (flowerbed[i+1] == 0);
+            bool emptyRight = (i == size-1) || (flowerbed[i+1] == 0);
             if (flowerbed[i] == 0 && emptyLeft && emptyRight) {
-                legalCnt++;
                 flowerbed[i] = 1;
+                placable++;
             }
         }
-        return (legalCnt >= n);
+        return placable >= n;
     }
 };
